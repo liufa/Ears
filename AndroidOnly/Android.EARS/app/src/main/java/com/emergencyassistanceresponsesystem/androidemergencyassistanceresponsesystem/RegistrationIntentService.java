588ab95a8daa89a4d428fa13109c4eec7a9d7057
liufa <mataswork@gmail.com>
2016-01-12 23:15:56 +0000

    Added test SQL script and progressed with GCS

--- AndroidOnly/Android.EARS/app/src/main/java/com/emergencyassistanceresponsesystem/androidemergencyassistanceresponsesystem/RegistrationIntentService.java
+++ AndroidOnly/Android.EARS/app/src/main/java/com/emergencyassistanceresponsesystem/androidemergencyassistanceresponsesystem/RegistrationIntentService.java
@@ -19,9 +19,10 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v4.content.LocalBroadcastManager;
+//import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.emergencyassistanceresponsesystem.androidemergencyassistanceresponsesystem.R;
 import com.google.android.gms.gcm.GcmPubSub;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
@@ -63,17 +64,17 @@ public class RegistrationIntentService extends IntentService {
             // You should store a boolean that indicates whether the generated token has been
             // sent to your server. If the boolean is false, send the token to your server,
             // otherwise your server should have already received the token.
-            sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, true).apply();
+           // sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, true).apply();
             // [END register_for_gcm]
         } catch (Exception e) {
             Log.d(TAG, "Failed to complete token refresh", e);
             // If an exception happens while fetching the new token or updating our registration data
             // on a third-party server, this ensures that we'll attempt the update at a later time.
-            sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false).apply();
+          //  sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false).apply();
         }
         // Notify UI that registration has completed, so the progress indicator can be hidden.
-        Intent registrationComplete = new Intent(QuickstartPreferences.REGISTRATION_COMPLETE);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+     //   Intent registrationComplete = new Intent(QuickstartPreferences.REGISTRATION_COMPLETE);
+       // LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
     }
 
     /**
